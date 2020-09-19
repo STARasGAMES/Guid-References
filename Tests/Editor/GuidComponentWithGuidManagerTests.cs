a003b560f77405fb0fcbce65a5e3c0e62449ea6a
STARasGAMES <doterpoter@gmail.com>
2020-09-19 14:20:58 +0300

    dev. Refactored GuidManager so that it does not require to use GuidComponent. Fixed tests after refactor. Added new tests.

---
+++ Tests/Editor/GuidComponentWithGuidManagerTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+namespace SaG.GuidReferences.Tests.Editor
+{
+    public class GuidComponentWithGuidManagerTests
+    {
+        
+        private GuidManagerMock guidManagerMock;
+        
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            guidManagerMock = new GuidManagerMock();
+            GuidManagerSingleton.SetInstance(guidManagerMock);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            GuidManagerSingleton.SetInstance(new GuidManager());
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            guidManagerMock.ClearArguments();
+        }
+
+        [Test]
+        public void GuidComponent_AddsToGuidManager_WhenInstantiated()
+        {
+            var guidComponent = GuidComponentTests.CreateNewGuid();
+            Assert.AreEqual(guidComponent.GetGuid(), guidManagerMock.AddGuidArgument);
+            Assert.AreEqual(guidComponent.gameObject, guidManagerMock.AddGameObjectArgument);
+        }
+        
+        [Test]
+        public void GuidComponent_RemovesToGuidManager_WhenDestroyed()
+        {
+            var guidComponent = GuidComponentTests.CreateNewGuid();
+            var guid = guidComponent.GetGuid();
+            Object.DestroyImmediate(guidComponent.gameObject);
+            Assert.AreEqual(guid, guidManagerMock.RemoveGuidArgument);
+        }
+        
+        // todo test when IGuidManager.Add() returns false
+    }
+}
--- Tests/Editor/GuidManagerMock.cs
+++ Tests/Editor/GuidManagerMock.cs
@@ -5,6 +5,18 @@ namespace SaG.GuidReferences.Tests.Editor
 {
     public class GuidManagerMock : IGuidManager
     {
+        /// <summary>
+        /// Gets guid that was provided in last Add() method call.
+        /// </summary>
+        public Guid AddGuidArgument { get; private set; } = Guid.Empty;
+        /// <summary>
+        /// Gets gameObject that was provided in last Add() method call.
+        /// </summary>
+        public GameObject AddGameObjectArgument { get; private set; } = null;
+        /// <summary>
+        /// Gets guid that was provided in last Remove() method call.
+        /// </summary>
+        public Guid RemoveGuidArgument { get; private set; } = Guid.Empty;
         public bool AddResult { get; set; } = true;
         public bool RemoveResult { get; set; } = true;
         public GameObject ResolveGuidResult { get; set; } = null;
@@ -22,13 +34,16 @@ namespace SaG.GuidReferences.Tests.Editor
             onRemoveCallback?.Invoke();
         }
         
-        public bool Add(GuidComponent guidComponent)
+        public bool Add(Guid guid, GameObject gameObject)
         {
+            AddGuidArgument = guid;
+            AddGameObjectArgument = gameObject;
             return AddResult;
         }
 
         public bool Remove(Guid guid)
         {
+            RemoveGuidArgument = guid;
             return RemoveResult;
         }
 
@@ -37,6 +52,13 @@ namespace SaG.GuidReferences.Tests.Editor
             this.onAddCallback = onAddCallback;
             this.onRemoveCallback = onRemoveCallback;
             return ResolveGuidResult;
+        }
+
+        public void ClearArguments()
+        {
+            AddGuidArgument = Guid.Empty;
+            AddGameObjectArgument = null;
+            RemoveGuidArgument = Guid.Empty;
         }
     }
 }
